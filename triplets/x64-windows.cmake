bb3f0efbf9f69a1396258f398128c85252d517ea
koki <26000062@qq.com>
2020-12-05 10:46:17 +0800

    glog gflags folly静态库

--- triplets/x64-windows.cmake
+++ triplets/x64-windows.cmake
@@ -2,10 +2,11 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
-if(${PORT} MATCHES "protobuf|pthread")
+if(${PORT} MATCHES "protobuf|pthread|glog|gflags|folly")
   set(VCPKG_CRT_LINKAGE dynamic)
   set(VCPKG_LIBRARY_LINKAGE static)
 else()
   set(VCPKG_CRT_LINKAGE dynamic)
   set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
+                  
