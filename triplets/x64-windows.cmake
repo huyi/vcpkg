f8fcd0f0dcf19f2e715b001813bf5116a45b3602
HuYi <huyi@cgws.com>
2021-01-06 12:07:35 +0800

    由于protobuf在3.14版本中静态链接编译出错，改为动态链接方式

--- triplets/x64-windows.cmake
+++ triplets/x64-windows.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
-if(${PORT} MATCHES "protobuf|pthread|glog|gflags|folly")
+if(${PORT} MATCHES "pthread|glog|gflags|folly")
   set(VCPKG_CRT_LINKAGE dynamic)
   set(VCPKG_LIBRARY_LINKAGE static)
 else()
