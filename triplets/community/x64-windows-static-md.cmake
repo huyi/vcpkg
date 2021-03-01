ff38ceafbfb3eb1bf4395bc6a0a699fc78d4d4db
HuYi <huyi@cgws.com>
2021-03-01 09:20:35 +0800

    Update

--- triplets/community/x64-windows-static-md.cmake
+++ triplets/community/x64-windows-static-md.cmake
@@ -3,5 +3,6 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 if(${PORT} MATCHES "mongo-c*|libbson")
+  set(VCPKG_CRT_LINKAGE dynamic)
   set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
